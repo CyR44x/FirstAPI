c3caca007323ceb108844cb593722b0a7e7c3a6e
Ilya Shmonin <ilya.shmonin2020@gmail.com>
2024-09-23 10:29:22 +0300

    add MessageHandler

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var message requestBody
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintf(w, "hello, %v", message)
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewDecoder(r.Body).Decode(&requestBody{})
 }
 
 func main() {
 	router := mux.NewRouter()
 	// наше приложение будет слушать запросы на localhost:8080/api/hello
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", MessageHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
