66d3d7f0c82f6e4b4b962a11f5b18cbbf0dd175d
CyR44x <ilya.shmonin2020@gmail.com>
2024-10-02 02:02:58 +0300

    add DeleteMessage

--- main.go
+++ main.go
@@ -33,6 +33,15 @@ func UpdateMessage(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&message)
 }
 
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	var message Message
+	//var id requestBody
+	//json.NewDecoder(r.Body).Decode(&id)
+	DB.Delete(&message, "ID")
+	json.NewEncoder(w).Encode(&message)
+
+}
+
 func main() {
 	// Вызываем метод InitDB() из файла db.go
 	InitDB()
@@ -44,5 +53,6 @@ func main() {
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessages).Methods("GET")
 	router.HandleFunc("/api/messages", UpdateMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages", DeleteMessage).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
