d6b737c155cc3a5c138ee885360870c9ff15ace4
CyR44x <ilya.shmonin2020@gmail.com>
2024-10-14 23:11:20 +0300

    add echo

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,8 +4,10 @@ import (
 	"RestApi/internal/database"
 	"RestApi/internal/handlers"
 	"RestApi/internal/messagesService"
-	"github.com/gorilla/mux"
-	"net/http"
+	"RestApi/internal/web/messages"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
 )
 
 func main() {
@@ -17,11 +19,27 @@ func main() {
 
 	handler := handlers.NewHandler(service)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/api/post", handler.CreateMessageHandler).Methods("POST")
-	router.HandleFunc("/api/get", handler.GetMessagesHandler).Methods("GET")
-	router.HandleFunc("/api/patch/{id}", handler.UpdateMessageHandler).Methods("PATCH")
-	router.HandleFunc("/api/delete/{id}", handler.DeleteMessageHandler).Methods("DELETE")
+	// Инициализируем echo
+	e := echo.New()
 
-	http.ListenAndServe(":8080", router)
+	// используем Logger и Recover
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Прикол для работы в echo. Передаем и регистрируем хендлер в echo
+	strictHandler := messages.NewStrictHandler(handler, nil) // тут будет ошибка
+	messages.RegisterHandlers(e, strictHandler)
+
+	if err := e.Start(":8080"); err != nil {
+		log.Fatalf("failed to start with err: %v", err)
+	}
 }
+
+//router := mux.NewRouter()
+//router.HandleFunc("/api/post", handler.CreateMessageHandler).Methods("POST")
+//router.HandleFunc("/api/get", handler.GetMessagesHandler).Methods("GET")
+//router.HandleFunc("/api/patch/{id}", handler.UpdateMessageHandler).Methods("PATCH")
+//router.HandleFunc("/api/delete/{id}", handler.DeleteMessageHandler).Methods("DELETE")
+
+//http.ListenAndServe(":8080", router)
+//}
--- internal/handlers/api_handlers.go
+++ internal/handlers/api_handlers.go
@@ -2,9 +2,10 @@ package handlers
 
 import (
 	"RestApi/internal/messagesService" // Импортируем наш сервис
+	"RestApi/internal/web/messages"
+	"context"
 	"encoding/json"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -13,45 +14,98 @@ type Handler struct {
 	Service *messagesService.MessageService
 }
 
+//func (h Handler) GetMessages(ctx context.Context, request messages.GetMessagesRequestObject) (messages.GetMessagesResponseObject, error) {
+//TODO implement me
+//panic("implement me")
+//}
+
+//func (h Handler) PostMessages(ctx context.Context, request messages.PostMessagesRequestObject) (messages.PostMessagesResponseObject, error) {
+//TODO implement me
+//panic("implement me")
+//}
+
 // Нужна для создания структуры Handler на этапе инициализации приложения
 
 func NewHandler(service *messagesService.MessageService) *Handler {
 	return &Handler{Service: service}
 }
 
-func (h *Handler) GetMessagesHandler(w http.ResponseWriter, r *http.Request) {
-	messages, err := h.Service.GetAllMessages()
+func (h *Handler) GetMessages(_ context.Context, _ messages.GetMessagesRequestObject) (messages.GetMessagesResponseObject, error) {
+	// Получение всех сообщений из сервиса
+	allMessages, err := h.Service.GetAllMessages()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(messages)
+
+	// Создаем переменную респон типа 200джейсонРеспонс
+	// Которую мы потом передадим в качестве ответа
+	response := messages.GetMessages200JSONResponse{}
+
+	// Заполняем слайс response всеми сообщениями из БД
+	for _, msg := range allMessages {
+		message := messages.Message{
+			Id:      &msg.ID,
+			Message: &msg.Text,
+		}
+		response = append(response, message)
+	}
+
+	// САМОЕ ПРЕКРАСНОЕ. Возвращаем просто респонс и nil!
+	return response, nil
 }
 
-func (h *Handler) CreateMessageHandler(w http.ResponseWriter, r *http.Request) {
-	var message messagesService.Message
-	err := json.NewDecoder(r.Body).Decode(&message)
-	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-	if message.Text == "" {
-		http.Error(w, "Message text is required", http.StatusBadRequest)
-		return
-	}
+//func (h *Handler) GetMessagesHandler(w http.ResponseWriter, r *http.Request) {
+//messages, err := h.Service.GetAllMessages()
+//if err != nil {
+//http.Error(w, err.Error(), http.StatusInternalServerError)
+//return
+//}
+//w.Header().Set("Content-Type", "application/json")
+//json.NewEncoder(w).Encode(messages)
+//}
 
-	createdMessage, err := h.Service.CreateMessage(message)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	log.Printf("Передаем в БД %v", createdMessage)
+func (h *Handler) PostMessages(_ context.Context, request messages.PostMessagesRequestObject) (messages.PostMessagesResponseObject, error) {
+	// Распаковываем тело запроса напрямую, без декодера!
+	messageRequest := request.Body
+	// Обращаемся к сервису и создаем сообщение
+	messageToCreate := messagesService.Message{Text: *messageRequest.Message}
+	createdMessage, err := h.Service.CreateMessage(messageToCreate)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(createdMessage)
+	if err != nil {
+		return nil, err
+	}
+	// создаем структуру респонс
+	response := messages.PostMessages201JSONResponse{
+		Id:      &createdMessage.ID,
+		Message: &createdMessage.Text,
+	}
+	// Просто возвращаем респонс!
+	return response, nil
 }
+
+//func (h *Handler) CreateMessageHandler(w http.ResponseWriter, r *http.Request) {
+//var message messagesService.Message
+//err := json.NewDecoder(r.Body).Decode(&message)
+//if err != nil {
+//http.Error(w, "Invalid input", http.StatusBadRequest)
+//return
+//}
+//if message.Text == "" {
+//http.Error(w, "Message text is required", http.StatusBadRequest)
+//return
+//}
+
+//createdMessage, err := h.Service.CreateMessage(message)
+//if err != nil {
+//http.Error(w, err.Error(), http.StatusInternalServerError)
+//return
+//}
+//log.Printf("Передаем в БД %v", createdMessage)
+
+//w.Header().Set("Content-Type", "application/json")
+//w.WriteHeader(http.StatusCreated)
+//json.NewEncoder(w).Encode(createdMessage)
+//}
 
 func (h *Handler) UpdateMessageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
